2c23d0982d59db12a2db3d51cbb4bd6509baa48b
CathalDolan <cathal@gizagig.com>
2020-07-13 12:56:11 +0000

    Ingredients Page - Measurement and Description corrected. Expand not working though. Styling to be completed

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,23 +2,6 @@ $(document).ready(function(){
     console.log("Hello World!");
 });
 
-// Required to allow sections collapse and expand. Taken from w3schools
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) { 
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
-
-
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
@@ -38,7 +21,7 @@ function goBack() {
   window.history.back();
 }
 
-/* Funtion to allow the "Advance" section in Search expand and collapes upwards */
+/* Funtion to allow the "Advance" section in Search expand and collape upwards */
 $(".header").click(function() {
   $('.content1').slideToggle().toggleClass('active');
   if ($('.content1').hasClass('active')) {
