11ada9db4bd4f7c39c5be07871cc936d07c15db4
CathalDolan <cathal@gizagig.com>
2020-07-16 15:45:53 +0000

    New Dish and New Portion pages created. Nutrition sections text centred.

--- assets/js/js.js
+++ assets/js/js.js
@@ -30,3 +30,23 @@ $(".header").click(function() {
     $('.header').text('Advanced');
   }
 });
+
+
+/* Funtion to allow the text wrap within a textarea input. From SpyYk3 http://jsfiddle.net/SpYk3/m8Qk9/ */
+$(function() {
+    //  changes mouse cursor when highlighting lower right of box
+    $(document).on('mousemove', 'textarea', function(e) {
+		var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
+			b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
+		$(this).css({
+			cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
+		});
+	})
+    //  the following simple make the textbox "Auto-Expand" as it is typed in
+    .on('keyup', 'textarea', function(e) {
+        //  the following will help the text expand as typing takes place
+        while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height()+1);
+        };
+    });
+});
